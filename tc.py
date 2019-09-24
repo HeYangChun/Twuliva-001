ac11f20d49b3a00ab99cfd1ec097e479fe48987c
heyc <yangchun_he@qq.com>
2019-09-24 21:19:31 +0800

    flip is added

--- tc.py
+++ tc.py
@@ -12,13 +12,14 @@ if not cap.isOpened():
     exit()
 while (1):
     _, frame = cap.read()
+    # frame = cv.flip(frame,2)
     hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
     lower_blue = np.array([110,50, 50])
     upper_blue = np.array([130,255,255])
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     res = cv.bitwise_and(frame, frame, mask = mask)
     cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
+    # cv.imshow('mask',mask)
     cv.imshow('res',res)
     k = cv.waitKey(5) & 0xFF
     if k == ord('q'):
