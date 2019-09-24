1f6145c95132fa5976a2a86914c66374d25e21a6
heyc <yangchun_he@qq.com>
2019-09-24 20:59:12 +0800

    color space convert

--- tc.py
+++ tc.py
@@ -1,29 +1,26 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread('/home/andy/1.jpeg')
-#px is like this:[b,g,r]
-px = img[100,100]
-#access only blue
-blue = img[100,100,0]
-#modify pixel
-img[100,100] = [255,255,255]
-#array method item and itemset
-img.item(10,10,2)
-img.itemset((10,10,2),100)
-#image attribute
-img.shape
-img.size
-img.dtype
-#ROI Region of Intresting
-# anything = img[300:400,200:400]
-# img[0:100, 0:200] = anything
-#split color channel
-b,g,r = cv.split(img)
-img = cv.merge((r,g,b))
-#or
-b = img[:,:,0]
+#Tips
+flags = [i for i in dir(cv) if i.startswith('COLOR')]
+print(flags)
+#for HSV , different software use diffenect scales, here, h:[0,179],S,V[0,255]
 
-cv.imshow("WINDOW TITLE",img)
-cv.waitKey(0)
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Failed to open, exit")
+    exit()
+while (1):
+    _, frame = cap.read()
+    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+    lower_blue = np.array([110,50, 50])
+    upper_blue = np.array([130,255,255])
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    res = cv.bitwise_and(frame, frame, mask = mask)
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask)
+    cv.imshow('res',res)
+    k = cv.waitKey(5) & 0xFF
+    if k == ord('q'):
+        break
 cv.destroyAllWindows()
--- tc_001.py
+++ tc_001.py
@@ -266,8 +266,32 @@ print(t)
 
 #. Never make copies of array unless it is needed. Try to use views instead. Array
 #  copying is a costly operation.
-#############################################################################
 
+#############################################################################
+# Changing Colorspaces
+#Tips
+flags = [i for i in dir(cv) if i.startswith('COLOR')]
+print(flags)
+#for HSV , different software use diffenect scales, here, h:[0,179],S,V[0,255]
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Failed to open, exit")
+    exit()
+while (1):
+    _, frame = cap.read()
+    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+    lower_blue = np.array([110,50, 50])
+    upper_blue = np.array([130,255,255])
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    res = cv.bitwise_and(frame, frame, mask = mask)
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask)
+    cv.imshow('res',res)
+    k = cv.waitKey(5) & 0xFF
+    if k == ord('q'):
+        break
+cv.destroyAllWindows()
 
 #############################################################################
 
