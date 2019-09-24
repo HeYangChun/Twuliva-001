ddef0738eb83c628930b1edc9987e2a1c6386342
heyc <yangchun_he@qq.com>
2019-09-24 20:31:40 +0800

    performance note

--- tc_001.py
+++ tc_001.py
@@ -216,3 +216,63 @@ img1 = cv.resize(img1,(0,0),fx=0.1,fy=0.1,interpolation=cv.INTER_NEAREST)
 cv.imshow('Dst', img1)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+#############################################################################
+
+#measure the performance of code
+e1 = cv.getTickCount()
+print("Task running")
+e2 = cv.getTickCount()
+time = (e2 - e1)/cv.getTickFrequency()
+
+#check optimized
+cv.setUseOptimized(True)
+img1 = cv.imread('/home/andy/logo.jpeg')
+e1 = cv.getTickCount()
+for i in range(5,49,20):
+    img1 = cv.medianBlur(img1,i)
+e2 = cv.getTickCount()
+t = (e2 - e1)/cv.getTickFrequency()
+print(t)
+
+#check optimized
+cv.setUseOptimized(False)
+img2 = cv.imread('/home/andy/logo.jpeg')
+e1 = cv.getTickCount()
+for i in range(5,49,20):
+    img2 = cv.medianBlur(img2,i)
+e2 = cv.getTickCount()
+t = (e2 - e1)/cv.getTickFrequency()
+print(t)
+
+#Use %timeit in python console
+#use timeit in code
+from timeit import timeit
+def func(x,y):
+    return x+y*2
+
+t = timeit('func(5,6)','from __main__ import func',number=10000000)
+print(t)
+
+#Performance optimizatioin techniques
+#try to implement the algorithm in a simple manner.
+#. Avoid using loops in Python as far as possible, especially double/triple loops etc.
+#  They are inherently slow.
+
+#. Vectorize the algorithm/code to the maximum possible extent because Numpy and OpenCV
+#  are optimized for vector operations.
+
+#. Exploit the cache coherence.
+
+#. Never make copies of array unless it is needed. Try to use views instead. Array
+#  copying is a costly operation.
+#############################################################################
+
+
+#############################################################################
+
+
+#############################################################################
+
+
+#############################################################################
