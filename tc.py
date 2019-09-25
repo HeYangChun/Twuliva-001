78efff8a4400edcfd727744aa8f435165975fc2e
heyc <yangchun_he@qq.com>
2019-09-25 21:47:52 +0800

    wrap affine,wrap perspective transform

--- tc.py
+++ tc.py
@@ -1,27 +1,52 @@
 import numpy as np
 import cv2 as cv
 
-#Tips
-flags = [i for i in dir(cv) if i.startswith('COLOR')]
-print(flags)
-#for HSV , different software use diffenect scales, here, h:[0,179],S,V[0,255]
+def showImage(title, img):
+    cv.imshow(title, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Failed to open, exit")
-    exit()
-while (1):
-    _, frame = cap.read()
-    # frame = cv.flip(frame,2)
-    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
-    lower_blue = np.array([110,50, 50])
-    upper_blue = np.array([130,255,255])
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
-    res = cv.bitwise_and(frame, frame, mask = mask)
-    cv.imshow('frame',frame)
-    # cv.imshow('mask',mask)
-    cv.imshow('res',res)
-    k = cv.waitKey(5) & 0xFF
-    if k == ord('q'):
-        break
-cv.destroyAllWindows()
+img = cv.imread('/home/andy/1.jpeg')
+res = cv.resize(img, None, fx=0.1, fy=0.1, interpolation=cv.INTER_CUBIC)
+# showImage('res',res)
+# or
+height, width = img.shape[:2]
+res = cv.resize(img, (int(0.2 * width), int(0.2 * height)), interpolation=cv.INTER_CUBIC)
+# showImage('res',res)
+
+rows, cols = res.shape[:2]
+# M = [1 0 100
+#     0 1 50]
+M = np.float32([[0.8, 0, 10], [0, 0.8, 10]])
+dst = cv.warpAffine(res, M, (cols, rows))
+# showImage('dst',dst)
+
+# rotation
+# M= [cosA   -sinA
+#    sinA   cosA]
+M = cv.getRotationMatrix2D((cols / 2, rows / 2), 360, 0.6)
+dst = cv.warpAffine(res, M, (cols, rows))
+# showImage('dst',dst)
+
+# affine transformation
+from matplotlib import pyplot as plt
+
+pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
+pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
+M = cv.getAffineTransform(pts1, pts2)
+dst = cv.warpAffine(res, M, (cols, rows))
+#why color changed using below plt.imshow()?
+# plt.subplot(121), plt.imshow(res), plt.title('input')
+# plt.subplot(122), plt.imshow(dst), plt.title('output')
+# plt.show()
+# showImage('input',res)
+# showImage('output',dst)
+
+#
+rows, cols, ch = res.shape
+pts1 = np.float32([[56, 65], [368, 52], [28, 387],[389,390]])
+pts2 = np.float32([[ 0,  0], [300,  0], [ 0, 300],[300,300]])
+M = cv.getPerspectiveTransform(pts1, pts2)
+dst = cv.warpPerspective(res, M, (300, 300))
+showImage('input',res)
+showImage('output',dst)
--- tc_001.py
+++ tc_001.py
@@ -272,6 +272,10 @@ print(t)
 #Tips
 flags = [i for i in dir(cv) if i.startswith('COLOR')]
 print(flags)
+#change  a color value.
+green = np.uint8([[[0,255,0]]])  #an image with only one pixel
+hsv_green = cv.cvtColor(green,cv.COLOR_BGR2HSV)
+
 #for HSV , different software use diffenect scales, here, h:[0,179],S,V[0,255]
 
 cap = cv.VideoCapture(0)
@@ -288,13 +292,63 @@ while (1):
     cv.imshow('frame',frame)
     cv.imshow('mask',mask)
     cv.imshow('res',res)
+    #How to remove the noises?TBD
     k = cv.waitKey(5) & 0xFF
     if k == ord('q'):
         break
 cv.destroyAllWindows()
-
 #############################################################################
+#Image transfomations
+# cv.warpAffine and cv.warpPerspective()
+def showImage(title, img):
+    cv.imshow(title, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
+img = cv.imread('/home/andy/1.jpeg')
+res = cv.resize(img, None, fx=0.1, fy=0.1, interpolation=cv.INTER_CUBIC)
+# showImage('res',res)
+# or
+height, width = img.shape[:2]
+res = cv.resize(img, (int(0.2 * width), int(0.2 * height)), interpolation=cv.INTER_CUBIC)
+# showImage('res',res)
+
+rows, cols = res.shape[:2]
+# M = [1 0 100
+#     0 1 50]
+M = np.float32([[0.8, 0, 10], [0, 0.8, 10]])
+dst = cv.warpAffine(res, M, (cols, rows))
+# showImage('dst',dst)
+
+# rotation
+# M= [cosA   -sinA
+#    sinA   cosA]
+M = cv.getRotationMatrix2D((cols / 2, rows / 2), 360, 0.6)
+dst = cv.warpAffine(res, M, (cols, rows))
+# showImage('dst',dst)
+
+# affine transformation
+from matplotlib import pyplot as plt
+
+pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
+pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
+M = cv.getAffineTransform(pts1, pts2)
+dst = cv.warpAffine(res, M, (cols, rows))
+#why color changed using below plt.imshow()?
+# plt.subplot(121), plt.imshow(res), plt.title('input')
+# plt.subplot(122), plt.imshow(dst), plt.title('output')
+# plt.show()
+# showImage('input',res)
+# showImage('output',dst)
+
+#
+rows, cols, ch = res.shape
+pts1 = np.float32([[56, 65], [368, 52], [28, 387],[389,390]])
+pts2 = np.float32([[ 0,  0], [300,  0], [ 0, 300],[300,300]])
+M = cv.getPerspectiveTransform(pts1, pts2)
+dst = cv.warpPerspective(res, M, (300, 300))
+showImage('input',res)
+showImage('output',dst)
 
 #############################################################################
 
