c58df4f1d38ab34c72b1c722c2ed30ee9e019ed7
heyc <yangchun_he@qq.com>
2019-09-26 21:23:48 +0800

    threshold part1

--- tc.py
+++ tc.py
@@ -1,52 +1,38 @@
 import numpy as np
 import cv2 as cv
 
-def showImage(title, img):
-    cv.imshow(title, img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
-img = cv.imread('/home/andy/1.jpeg')
-res = cv.resize(img, None, fx=0.1, fy=0.1, interpolation=cv.INTER_CUBIC)
-# showImage('res',res)
-# or
-height, width = img.shape[:2]
-res = cv.resize(img, (int(0.2 * width), int(0.2 * height)), interpolation=cv.INTER_CUBIC)
-# showImage('res',res)
-
-rows, cols = res.shape[:2]
-# M = [1 0 100
-#     0 1 50]
-M = np.float32([[0.8, 0, 10], [0, 0.8, 10]])
-dst = cv.warpAffine(res, M, (cols, rows))
-# showImage('dst',dst)
-
-# rotation
-# M= [cosA   -sinA
-#    sinA   cosA]
-M = cv.getRotationMatrix2D((cols / 2, rows / 2), 360, 0.6)
-dst = cv.warpAffine(res, M, (cols, rows))
-# showImage('dst',dst)
-
-# affine transformation
 from matplotlib import pyplot as plt
+#Imgae threashold
 
-pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
-pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
-M = cv.getAffineTransform(pts1, pts2)
-dst = cv.warpAffine(res, M, (cols, rows))
-#why color changed using below plt.imshow()?
-# plt.subplot(121), plt.imshow(res), plt.title('input')
-# plt.subplot(122), plt.imshow(dst), plt.title('output')
-# plt.show()
-# showImage('input',res)
-# showImage('output',dst)
+img = cv.imread('/home/andy/sudoku.png',0)
+# img = cv.medianBlur(img,5)
+ret, th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,    cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
+titles = ['Org','Glb','ApaM','ApaG']
+imgs = [img, th1,th2,th3]
+for i in range(4):
+    plt.subplot(2,3,i+1),plt.imshow(imgs[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
 
-#
-rows, cols, ch = res.shape
-pts1 = np.float32([[56, 65], [368, 52], [28, 387],[389,390]])
-pts2 = np.float32([[ 0,  0], [300,  0], [ 0, 300],[300,300]])
-M = cv.getPerspectiveTransform(pts1, pts2)
-dst = cv.warpPerspective(res, M, (300, 300))
-showImage('input',res)
-showImage('output',dst)
+plt.show()
+exit()
+
+#if val > threshold-h val=high else val = valTBD
+# diffenec betwwen these effects
+img = cv.imread('/home/andy/gradient.jpg')
+ret, thresh1 = cv.threshold(img,72,96,cv.THRESH_BINARY)
+ret, thresh2 = cv.threshold(img,72,96,cv.THRESH_BINARY_INV)
+ret, thresh3 = cv.threshold(img,72,96,cv.THRESH_TRUNC)
+ret, thresh4 = cv.threshold(img,72,96,cv.THRESH_TOZERO)
+ret, thresh5 = cv.threshold(img,72,96,cv.THRESH_TOZERO_INV)
+
+titles = ['Org','Bin','IBin','Trc','TZero','ITZero']
+imgs = [img,thresh1,thresh2,thresh3,thresh4,thresh5]
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(imgs[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+
+plt.show()
