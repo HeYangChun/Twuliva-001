fc3ec896244a44b3bacd2224f551c47fcf7044a5
heyc <yangchun_he@qq.com>
2019-10-04 20:48:07 +0800

    histgram

--- tc.py
+++ tc.py
@@ -2,20 +2,39 @@ import numpy as np, sys
 import cv2 as cv
 import matplotlib.pyplot as plt
 #############################################################################
-#contour
-img = cv .imread('/home/andy/apple.png')
-imggray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-ret,thresh = cv.threshold(imggray,144,255,0)
-#old ver im2,contours,hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-contours,hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-#draw contour
-#draw all contour
-cv.drawContours(img,contours,-1,(0,0,255), 1)
-#draw an individual contour, ex, 4th
-# cv.drawContours(img,contours, 9, (0,0,255), 1)
-#useful
-# cnt = contours[4]
-# cv.drawContours(imggray,[cnt], 0, (0,255,0),3)
-cv.imshow('image and contours',img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+#Histogram
+# an overall idea about the intensity distribution of an imag
+img = cv .imread('/home/andy/apple1.png',0)
+hist, bin = np.histogramk(img.flatten(),256,[0,256])
+cdf = hist.cumsum()
+cdf_normalized = cdf * hist.max()/ cdf.max()
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+
+cdf_m = np.ma.masked_equal(cdf,0)
+cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+cdf = np.ma.filled(cdf_m,0).astype('uint8')
+img2 = cdf[img]
+plt.subplot(131),plt.imshow(img)
+plt.subplot(132),plt.imshow(img2)
+plt.subplot(133)
+plt.plot(cdf_m, color = 'b')
+plt.hist(img2.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+
+equ = cv.equalizeHist(img)
+res = np.hstack((img,equ))
+cv.imwrite('/home/andy/res.png',res)
+#CLAHE
+img = cv.imread('/home/andy/tsukuba_l.png',0)
+clahe = cv.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+cl1 = clahe.apply(img)
+plt.subplot(121), plt.imshow(img,cmap='gray')
+plt.subplot(122), plt.imshow(cl1,cmap='gray')
+plt.show()
+cv.imwrite('/home/andy/tsu2.png',cl1)
--- tc_001.py
+++ tc_001.py
@@ -589,3 +589,70 @@ cv.drawContours(img,contours,-1,(0,0,255), 1)
 cv.imshow('image and contours',img)
 cv.waitKey(0)
 cv.destroyAllWindows()
+#############################################################################
+#Histogram
+# an overall idea about the intensity distribution of an imag
+img = cv .imread('/home/andy/apple1.png',0)
+# hist = cv.calcHist([img],[0],None,[256],[0,256])
+#np
+# hist, bin = np.histogram(img.ravel(),256,[0,256])
+#show
+# plt.hist(img.ravel(),256,[0,256])
+# plt.show()
+
+# color = ('b','g','r')
+# for i, col in enumerate(color):
+#     histr = cv.calcHist([img],[i],None,[256],[0,256])
+#     plt.plot(histr,color=col)
+#     # plt.xlim([0,256])
+# plt.show()
+#Find histograms of some regions of an image
+mask = np. zeros(img.shape[:2],np.uint8)
+mask [0:128, 0:256]=255
+masked_img = cv.bitwise_and(img, img, mask=mask)
+
+hist_full = cv.calcHist([img],[0],None,[256],[0,256])
+hist_mask = cv.calcHist([img],[0],mask,[256],[0,256])
+
+plt.subplot(221),plt.imshow(img,'gray')
+plt.subplot(222),plt.imshow(mask,'gray')
+plt.subplot(223),plt.imshow(masked_img,'gray')
+plt.subplot(224),plt.plot(hist_full),plt.plot(hist_mask)
+plt.xlim([0,256])
+plt.show()
+#Histogram
+# an overall idea about the intensity distribution of an imag
+img = cv .imread('/home/andy/apple1.png',0)
+hist, bin = np.histogram(img.flatten(),256,[0,256])
+cdf = hist.cumsum()
+cdf_normalized = cdf * hist.max()/ cdf.max()
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+
+cdf_m = np.ma.masked_equal(cdf,0)
+cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+cdf = np.ma.filled(cdf_m,0).astype('uint8')
+img2 = cdf[img]
+plt.subplot(131),plt.imshow(img)
+plt.subplot(132),plt.imshow(img2)
+plt.subplot(133)
+plt.plot(cdf_m, color = 'b')
+plt.hist(img2.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+
+equ = cv.equalizeHist(img)
+res = np.hstack((img,equ))
+cv.imwrite('/home/andy/res.png',res)
+#CLAHE
+img = cv.imread('/home/andy/tsukuba_l.png',0)
+clahe = cv.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+cl1 = clahe.apply(img)
+plt.subplot(121), plt.imshow(img,cmap='gray')
+plt.subplot(122), plt.imshow(cl1,cmap='gray')
+plt.show()
+cv.imwrite('/home/andy/tsu2.png',cl1)
